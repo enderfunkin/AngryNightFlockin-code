ce622b3c08a3e36ccb73a9fe59dc6f29ef285dfd
Ender Funkin <78095941+enderfunkin@users.noreply.github.com>
2022-05-13 15:52:15 +0600

    Upgraded

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -582,18 +582,14 @@ class TitleState extends MusicBeatState
 			{
 				case 1:
 					#if PSYCH_WATERMARKS
-					createCoolText(['Psych Engine by'], 15);
+					createCoolText(['This is mod by'], 15);
 					#else
 					createCoolText(['ninjamuffin99', 'phantomArcade', 'kawaisprite', 'evilsk8er']);
 					#end
 				// credTextShit.visible = true;
 				case 3:
 					#if PSYCH_WATERMARKS
-					addMoreText('Shadow Mario', 15);
-					addMoreText('RiverOaken', 15);
-					addMoreText('shubs', 15);
-					#else
-					addMoreText('present');
+					addMoreText('Unitech Studio', 15);
 					#end
 				// credTextShit.text += '\npresent...';
 				// credTextShit.addText();
@@ -604,12 +600,12 @@ class TitleState extends MusicBeatState
 				// credTextShit.screenCenter();
 				case 5:
 					#if PSYCH_WATERMARKS
-					createCoolText(['Not associated', 'with'], -40);
+					createCoolText(['Creator Angry Birds', 'by'], -40);
 					#else
 					createCoolText(['In association', 'with'], -40);
 					#end
 				case 7:
-					addMoreText('newgrounds', -40);
+					addMoreText('Rovio', -40);
 					ngSpr.visible = true;
 				// credTextShit.text += '\nNewgrounds';
 				case 8:
@@ -631,13 +627,13 @@ class TitleState extends MusicBeatState
 				// credTextShit.text = "Friday";
 				// credTextShit.screenCenter();
 				case 13:
-					addMoreText('Friday');
+					addMoreText('Angry');
 				// credTextShit.visible = true;
 				case 14:
 					addMoreText('Night');
 				// credTextShit.text += '\nNight';
 				case 15:
-					addMoreText('Funkin'); // credTextShit.text += '\nFunkin';
+					addMoreText('Flockin'); // credTextShit.text += '\nFunkin';
 
 				case 16:
 					skipIntro();
